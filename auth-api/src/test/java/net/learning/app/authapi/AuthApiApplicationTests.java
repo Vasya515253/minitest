1ac99c4a430b3e2897eb8886bed2f5a51e405130
Vasia515253 <paniuk_ak24@nuwm.edu.ua>
2026-09-20 09:36:06 +0300

    basic structure

---
+++ auth-api/src/test/java/net/learning/app/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learning.app.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ data-api/src/test/java/net/learning/app/dataapi/DataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learning.app.dataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
